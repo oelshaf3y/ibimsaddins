38d52f83d5dbe0be7a0eb88d7738bff9204564e4
oelshaf3y <o.elshaf3y@gmail.com>
2023-10-18 18:21:36 +0300

    place on ceiling init

---
+++ IBIMSGen/ElecEquipCeilings/AlignBetween2Pts.cs
@@ -0,0 +1,164 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IBIMSGen.ElecEquipCeilings
+{
+    [TransactionAttribute(TransactionMode.Manual)]
+    internal class AlignBetween2Pts : IExternalCommand
+    {
+        UIDocument uidoc;
+        Document doc;
+        Options options;
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            uidoc = commandData.Application.ActiveUIDocument;
+            doc = uidoc.Document;
+            options = new Options();
+            options.ComputeReferences = true;
+            if (doc.ActiveView is View3D) {TaskDialog.Show("Error", "Active View Can't be 3D!"); return Result.Failed; }
+            Element elem = doc.GetElement(uidoc.Selection.PickObject(ObjectType.Element, "Select Elements you'd like to center"));
+            using (Transaction transaction = new Transaction(doc, "Center Element"))
+            {
+
+                transaction.Start();
+
+                Solid solid = getSolid(elem);
+                EdgeArray edgeArray = solid.Edges;
+                List<XYZ> points=new List<XYZ>();
+                foreach(Edge edge in edgeArray)
+                {
+                    Curve curve = edge.AsCurve();
+                    points.Add(curve.GetEndPoint(0));
+                    points.Add(curve.GetEndPoint(1));
+                }
+                XYZ p0 = points.OrderBy(p=>p.DistanceTo(XYZ.Zero)).FirstOrDefault();
+                XYZ p00 = points.OrderByDescending(p=>p.DistanceTo(XYZ.Zero)).FirstOrDefault();
+                XYZ centroid = Line.CreateBound(p0, p00).Evaluate(0.5, true);
+
+                Plane plane = Plane.CreateByNormalAndOrigin(uidoc.ActiveView.ViewDirection, uidoc.ActiveView.Origin);
+
+                SketchPlane sp = SketchPlane.Create(doc, plane);
+
+                uidoc.ActiveView.SketchPlane = sp;
+
+                uidoc.ActiveView.ShowActiveWorkPlane();
+
+                XYZ origin = ((LocationPoint)elem.Location).Point;
+                //DirectShape.CreateElement(doc,new ElementId(BuiltInCategory.OST_GenericModel)).SetShape(new List<GeometryObject> { Line.CreateBound(XYZ.Zero, origin) });
+                GeometryElement geoelem = elem.get_Geometry(options);
+                foreach(GeometryObject obj in geoelem)
+                {
+
+                }
+
+                XYZ p1 = uidoc.Selection.PickPoint(Autodesk.Revit.UI.Selection.ObjectSnapTypes.Intersections, "Pick 1st corner.");
+                XYZ p2 = uidoc.Selection.PickPoint(Autodesk.Revit.UI.Selection.ObjectSnapTypes.Intersections, "Pick 2nd corner.");
+                XYZ p3 = new XYZ(p1.X, p1.Y, origin.Z);
+                XYZ p4 = new XYZ(p2.X, p2.Y, origin.Z);
+                XYZ newLocation;
+                try
+                {
+                    newLocation = Line.CreateBound(p1, p2).Evaluate(0.5, true);
+                }
+                catch
+                {
+                    newLocation = p3.Add((p4 - p3).GetLength() / 2 * (p4 - p3).Normalize());
+                }
+                StringBuilder sb = new StringBuilder();
+
+                XYZ delta = (newLocation - centroid);
+                XYZ dir = delta.Normalize();
+                sb.AppendLine(origin.ToString());
+                sb.AppendLine(newLocation.ToString());
+                sb.AppendLine(delta.ToString());
+                elem.Location.Move(delta);
+                sb.AppendLine(((LocationPoint)elem.Location).Point.ToString());
+                //TaskDialog.Show("info", sb.ToString());
+                transaction.Commit();
+                transaction.Dispose();
+            }
+            //TaskDialog.Show(" s", p.ToString());
+            return Result.Succeeded;
+        }
+
+        public Solid getSolid(Element elem)
+        {
+            IList<Solid> solids = new List<Solid>();
+            try
+            {
+
+                GeometryElement geo = elem.get_Geometry(options);
+                if (geo.FirstOrDefault() is Solid)
+                {
+                    Solid solid = (Solid)geo.FirstOrDefault();
+                    return SolidUtils.Clone(solid);
+                }
+                foreach (GeometryObject geometryObject in geo)
+                {
+                    if (geometryObject != null)
+                    {
+                        Solid solid = geometryObject as Solid;
+                        if (solid != null && solid.Volume > 0)
+                        {
+                            solids.Add(solid);
+
+                        }
+                    }
+                }
+            }
+            catch
+            {
+            }
+            if (solids.Count == 0)
+            {
+                try
+                {
+                    GeometryElement geo = elem.get_Geometry(options);
+                    GeometryInstance geoIns = geo.FirstOrDefault() as GeometryInstance;
+                    if (geoIns != null)
+                    {
+                        GeometryElement geoElem = geoIns.GetInstanceGeometry();
+                        if (geoElem != null)
+                        {
+                            foreach (GeometryObject geometryObject in geoElem)
+                            {
+                                Solid solid = geometryObject as Solid;
+                                if (solid != null && solid.Volume > 0)
+                                {
+                                    solids.Add(solid);
+                                }
+                            }
+                        }
+                    }
+                }
+                catch
+                {
+                    throw new InvalidOperationException();
+                }
+            }
+            if (solids.Count > 0)
+            {
+                try
+                {
+
+                    return SolidUtils.Clone(solids.OrderByDescending(x => x.Volume).ElementAt(0));
+                }
+                catch
+                {
+                    return solids.OrderByDescending(x => x.Volume).ElementAt(0);
+                }
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
---
+++ IBIMSGen/ElecEquipCeilings/CeilingLightsPlacement.cs
@@ -0,0 +1,162 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IBIMSGen.ElecEquipCeilings
+{
+    [TransactionAttribute(TransactionMode.Manual)]
+    internal class CeilingLightsPlacement : IExternalCommand
+    {
+        UIDocument uidoc;
+        Document doc;
+        List<FamilySymbol> Symbols;
+        FamilySymbol fs;
+        Options options;
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            uidoc = commandData.Application.ActiveUIDocument;
+            doc = uidoc.Document;
+            Symbols = new FilteredElementCollector(doc).OfCategory(BuiltInCategory.OST_LightingFixtures).OfClass(typeof(FamilySymbol)).Cast<FamilySymbol>().ToList();
+            fs = Symbols.Where(x => x.Name.Equals("CSL-1")).FirstOrDefault();
+            options = new Options();
+            options.ComputeReferences = true;
+            if (fs == null)
+            {
+                td("Not found FS");
+                return Result.Failed;
+            }
+            using (Transaction transaction = new Transaction(doc, "Center Element"))
+            {
+
+                transaction.Start();
+
+                #region set workplane
+                Plane plane = Plane.CreateByNormalAndOrigin(uidoc.ActiveView.ViewDirection, uidoc.ActiveView.Origin);
+                SketchPlane sp = SketchPlane.Create(doc, plane);
+                uidoc.ActiveView.SketchPlane = sp;
+                uidoc.ActiveView.ShowActiveWorkPlane();
+                #endregion
+                ElementMulticategoryFilter ceilingsAndFloors = new ElementMulticategoryFilter(new BuiltInCategory[] { BuiltInCategory.OST_Floors, BuiltInCategory.OST_Ceilings });
+                List<Element> elementInView = new FilteredElementCollector(doc, doc.ActiveView.Id).WherePasses(ceilingsAndFloors).ToList();
+                uidoc.Selection.SetElementIds(elementInView.Select(x => x.Id).ToArray());
+
+                XYZ p1 = uidoc.Selection.PickPoint(ObjectSnapTypes.Endpoints, "Pick 1st Corner");
+                XYZ p2 = uidoc.Selection.PickPoint(ObjectSnapTypes.Endpoints, "Pick 2nd Corner");
+                XYZ p3 = uidoc.Selection.PickPoint(ObjectSnapTypes.Endpoints, "Pick 3rd Corner");
+                Line l12 = Line.CreateBound(p1, p2);
+                Line l23 = Line.CreateBound(p2, p3);
+                XYZ p4 = p1.Add(l23.Length * l23.Direction);
+                double nx = 3;
+                double ny = 2;
+                double pitchX = l12.Length / nx;
+                double pitchY = l23.Length / ny;
+
+                Reference refFace = uidoc.Selection.PickObject(ObjectType.Face, "pick Host");
+                Element elem = doc.GetElement(refFace);
+                var par = elem.get_Parameter(BuiltInParameter.TILE_PATTERN_GRID_CELLS_X);
+                td(par.ToString)
+                Solid solid = getSolid(elem);
+                double z = solid.Faces.Cast<PlanarFace>().OrderBy(x => x.Origin.Z).Select(x => x.Origin.Z).First();
+
+                for (int i = 0; i < nx; i++)
+                {
+                    for (int j = 0; j < ny; j++)
+                    {
+                        XYZ point = p1.Add((pitchX / 2 + pitchX * i) * l12.Direction.Normalize()).Add((pitchY / 2 + pitchY * j) * l23.Direction.Normalize());
+                        XYZ cP = new XYZ(point.X, point.Y, z);
+                        doc.Create.NewFamilyInstance(refFace, cP, doc.ActiveView.ViewDirection.CrossProduct(XYZ.BasisX).Negate(), fs);
+                    }
+                }
+
+                transaction.Commit();
+                transaction.Dispose();
+            }
+
+
+
+            return Result.Succeeded;
+        }
+        void td(string message)
+        {
+            TaskDialog.Show("Message", message);
+        }
+        public Solid getSolid(Element elem)
+        {
+            IList<Solid> solids = new List<Solid>();
+            try
+            {
+
+                GeometryElement geo = elem.get_Geometry(options);
+                if (geo.FirstOrDefault() is Solid)
+                {
+                    Solid solid = (Solid)geo.FirstOrDefault();
+                    return SolidUtils.Clone(solid);
+                }
+                foreach (GeometryObject geometryObject in geo)
+                {
+                    if (geometryObject != null)
+                    {
+                        Solid solid = geometryObject as Solid;
+                        if (solid != null && solid.Volume > 0)
+                        {
+                            solids.Add(solid);
+
+                        }
+                    }
+                }
+            }
+            catch
+            {
+            }
+            if (solids.Count == 0)
+            {
+                try
+                {
+                    GeometryElement geo = elem.get_Geometry(options);
+                    GeometryInstance geoIns = geo.FirstOrDefault() as GeometryInstance;
+                    if (geoIns != null)
+                    {
+                        GeometryElement geoElem = geoIns.GetInstanceGeometry();
+                        if (geoElem != null)
+                        {
+                            foreach (GeometryObject geometryObject in geoElem)
+                            {
+                                Solid solid = geometryObject as Solid;
+                                if (solid != null && solid.Volume > 0)
+                                {
+                                    solids.Add(solid);
+                                }
+                            }
+                        }
+                    }
+                }
+                catch
+                {
+                    throw new InvalidOperationException();
+                }
+            }
+            if (solids.Count > 0)
+            {
+                try
+                {
+
+                    return SolidUtils.Clone(solids.OrderByDescending(x => x.Volume).ElementAt(0));
+                }
+                catch
+                {
+                    return solids.OrderByDescending(x => x.Volume).ElementAt(0);
+                }
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
